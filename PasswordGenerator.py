b6868b425e1dce71b36caf98afbd706d3c062de9
Advik G M <vaishnavi.dec04@gmail.com>
2026-05-11 12:30:27 +0530

    PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -3,10 +3,14 @@ import random
 letters = "abcdefghijklmnopqrstuvwxyz"
 numbers = "0123456789"
 symbols = "!@#$%^&*()"
+uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 length = int(input("Enter Password Length: "))
+if length <= 0:
+    print("Password length must be greater than 0")
+    exit()
 
-all_chars = letters + numbers + symbols
+all_chars = letters + numbers + symbols + uppercase
 
 password = ""
 
